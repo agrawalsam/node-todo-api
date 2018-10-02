208d6b78c87715ad71b965b09bc919e6ca8021a0
Srix <agrawalsam1997@gmail.com>
2018-10-03 00:42:44 +0530

    Added Updated file

--- playgrounds/mongodb-delete.js
+++ playgrounds/mongodb-delete.js
@@ -7,32 +7,32 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
     console.log("Connected to MongoDB Server");
 
     //deleteMany
-    // db.collection('Todos').deleteMany({
-    //     text:"Eat Lunch"
-    // }).then((result)=>{
-    //     console.log(result);
-    // })
+    db.collection('Todos').deleteMany({
+        text:"Eat Lunch"
+    }).then((result)=>{
+        console.log(result);
+    });
 
     //deleteOne
-    // db.collection('Todos').deleteOne({
-    //     text:"Eat lunch"
-    // }).then((result)=>{
-    //     console.log(result);
-    // })
+    db.collection('Todos').deleteOne({
+        text:"Eat lunch"
+    }).then((result)=>{
+        console.log(result);
+    });
 
     //findoneanddelete
-    // db.collection('Todos').findOneAndDelete({
-    //     completed:false
-    // }).then((result)=>{
-    //     console.log(result);
-    // })
+    db.collection('Todos').findOneAndDelete({
+        completed:false
+    }).then((result)=>{
+        console.log(result);
+    });
 
     //challenge
-    // db.collection('User').deleteMany({
-    //     name:"Saurav"
-    // }).then((result)=>{
-    //     console.log(result);
-    // })
+    db.collection('User').deleteMany({
+        name:"Saurav"
+    }).then((result)=>{
+        console.log(result);
+    });
 
     db.collection('User').findOneAndDelete({
         _id:new ObjectID("5bb39cb1f202b06f3a99f915")
