03062bbd225ae552f48a8ff6cea732e8e04dddc3
Srix <agrawalsam1997@gmail.com>
2018-10-08 02:10:33 +0530

    Adding /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose}=require('./db/mongoose');
 var {todo}=require('./models/todos');
@@ -26,6 +27,20 @@ app.get('/todos',(req,res )=>{
     });
 });
 
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    todo.findById(id).then((todos)=>{
+        if(!todos){
+            return res.status(400).send();
+        }
+        res.send({todos});
+    },(e)=>{
+        res.status(400).send();
+    });
+});
 app.listen(3000,()=>{
     console.log("Started on port :3000");
 })
